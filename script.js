179a8e0244a473a3ea63371a8efbe7e2add35e2a
Anastasiya <101666311+afrikantovaa@users.noreply.github.com>
2025-02-03 02:37:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,22 +21,4 @@ askButton.addEventListener('click', () => {
         answer.style.animation = 'textAppear 0.5s ease-in-out'; // Анимация появления текста
         ball.style.animation = ''; // Сбрасываем анимацию
     }, 500);
-
-    const starBackground = document.querySelector('.star-background');
-
-function createStar() {
-    const star = document.createElement('div');
-    star.classList.add('star');
-    star.style.width = `${Math.random() * 3}px`;
-    star.style.height = star.style.width;
-    star.style.left = `${Math.random() * 100}vw`;
-    star.style.top = `${Math.random() * 100}vh`;
-    star.style.animationDuration = `${Math.random() * 2 + 1}s`;
-    starBackground.appendChild(star);
-}
-
-// Создаем 100 звезд
-for (let i = 0; i < 100; i++) {
-    createStar();
-}
 });
